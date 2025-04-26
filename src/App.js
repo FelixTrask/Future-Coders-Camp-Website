857d53b3e28aecd10dc5f5c6d8cac5d084defc54
FelixTrask <felixtrask@gmail.com>
2025-04-25 22:20:49 -0400

    Removed CSS files + tested tailwind

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div class="text-center bg-gray">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
